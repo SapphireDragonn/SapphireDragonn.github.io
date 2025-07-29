30895f835e31cc4ea5be9f75f700104420b66250
SapphireDragonn <146882019+SapphireDragonn@users.noreply.github.com>
2025-07-29 18:54:40 +0000

    tailwind attempt 3

--- gunnika_portfolio/next.config.js
+++ gunnika_portfolio/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/gunnika_portfolio',
+  basePath: '/SapphireDragonn.github.io',
   trailingSlash: true,
 }
 
