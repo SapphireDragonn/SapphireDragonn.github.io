abe6c7ea90ba56ac0ea75c4431d50ffc8f1ab2fc
SapphireDragonn <146882019+SapphireDragonn@users.noreply.github.com>
2025-07-29 18:44:26 +0000

    tailwind attempt 2

--- gunnika_portfolio/next.config.js
+++ gunnika_portfolio/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/gunnika_portfolio',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig;
--- gunnika_portfolio/tailwing.config.js
+++ gunnika_portfolio/tailwing.config.js
@@ -1,7 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx,mdx}",
+    "./pages/**/*.{js,jsx,ts,tsx,mdx}",
+    "./components/**/*.{js,jsx,ts,tsx,mdx}",
   ],
   theme: {
     extend: {},
